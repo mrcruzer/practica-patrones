a5fa6acffb08693f30dfa82efc184d23094a33ed
Isaac Avelino <isaacavelino14@gmail.com>
2022-02-17 07:55:41 +0800

    Update EmpleadoController_Singleton.cs

--- EmpleadoController_Singleton.cs
+++ EmpleadoController_Singleton.cs
@@ -1,105 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using empresa_administrador_api.Models;
+using static System.Console;
 
-namespace empresa_administrador_api.Controllers
+namespace Singleton
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmpleadoController_Singleton : ControllerBase
+  class Program
+  {
+    static void Main(string[] args)
     {
-        private readonly ApplicationDbContext _context;
+      Singleton Manager = new Singleton();
+      Manager.Mensaje("Mensaje desde Manager");
 
-        public EmpleadoController_Singleton(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+      Singleton Empleado = new Singleton();
+      Empleado.Mensaje("Mensaje desde Empleado");
 
-       // Obtener Empleados
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Empleado>>> GetEmpleados()
-        {
-            return await _context.Empleados.ToListAsync();
-        }
-
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Empleado>> GetEmpleado(int id)
-        {
-            var empleado = await _context.Empleados.FindAsync(id);
-
-            if (empleado == null)
-            {
-                return NotFound();
-            }
-
-            return empleado;
-        }
-
-        // PUT: api/Empleado/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutEmpleado(int id, Empleado empleado)
-        {
-            if (id != empleado.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(empleado).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!EmpleadoExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-       
-        [HttpPost]
-        public async Task<ActionResult<Empleado>> PostEmpleado(Empleado empleado)
-        {
-            _context.Empleados.Add(empleado);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetEmpleado", new { id = empleado.Id }, empleado);
-        }
-
-        
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Empleado>> DeleteEmpleado(int id)
-        {
-            var empleado = await _context.Empleados.FindAsync(id);
-            if (empleado == null)
-            {
-                return NotFound();
-            }
-
-            _context.Empleados.Remove(empleado);
-            await _context.SaveChangesAsync();
-
-            return empleado;
-        }
-
-        private bool EmpleadoExists(int id)
-        {
-            return _context.Empleados.Any(e => e.Id == id);
-        }
+      ReadLine();
     }
+  }
 }
+
